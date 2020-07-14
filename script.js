7478b56c8fbb9b41975d6271b60e805dc495a04a
Jake <60721064+Jake-Derrick@users.noreply.github.com>
2020-07-14 18:42:36 -0500

    Added Calculate function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
-    $('input[type=radio]').click(function(){
-    var _value = this.value
+// Global variable, set to Easy because the radio button Easy is pre-selected.
+var _value = "Easy";
+
+$(document).ready(function() {
+  $('input[type=radio]').click(function() {
+    _value = this.value
 
     var EasyTable = document.getElementById('EasyTable');
     //Hides table when new radio button is selected
@@ -12,22 +15,69 @@ $(document).ready(function(){
     var HardTable = document.getElementById('HardTable')
     HardTable.style.display = 'none';
 
-        if(_value == "Easy")
-        {
-            // Displays table (div) when radio button is selected
-            EasyTable.style.display = 'block';
-        }
-        else if(_value == "Medium")
-        {
-            MediumTable.style.display = 'block';
-        }
-        else if(_value == "Hard")
-        {
-            HardTable.style.display = 'block';
-        }
-        else
-        {
-            //Custom option?
-        }
-    });
+    if (_value == "Easy") {
+      // Displays table (div) when radio button is selected
+      EasyTable.style.display = 'block';
+    } else if (_value == "Medium") {
+      MediumTable.style.display = 'block';
+    } else if (_value == "Hard") {
+      HardTable.style.display = 'block';
+    } else {
+      //Custom option?
+    }
+  });
 });
+
+function Calculate() {
+  var placement = document.getElementById("placement").value;
+  var kills = document.getElementById("kills").value;
+  var total = 0;
+  if (_value == "Easy") {
+    if (kills == 0) {
+      total += 5;
+    } else if (kills == 1) {
+      total += 2;
+    }
+    // 2 or more kills adds 0
+
+    if (placement >= 15) {
+      total += 5;
+    } else if (placement <= 14 && placement >= 10) {
+      total += 2;
+    }
+    // 9 or lower adds 0
+  } else if (_value == "Medium") {
+    if (kills == 0) {
+      total += 7;
+    } else if (kills == 1) {
+      total += 3;
+    }
+    // 2 or more kills adds 0
+
+    if (placement >= 15) {
+      total += 7;
+    } else if (placement <= 14 && placement >= 10) {
+      total += 3;
+    }
+    // 9 or lower adds 0
+  } else if (_value == "Hard") {
+    if (kills == 0) {
+      total += 10;
+    } else if (kills == 1) {
+      total += 5;
+    } else if (kills == 2) {
+      total += 3;
+    }
+    // 3 or more kills adds 0
+
+    if (placement >= 15) {
+      total += 10;
+    } else if (placement <= 14 && placement >= 10) {
+      total += 5;
+    } else if (placement <= 9 && placement >= 4) {
+      total += 3;
+    }
+    // 3 or lower adds 0
+  }
+  document.getElementById("total").innerHTML = total;
+}
